6d64b7861fedff54fcf04fb67c06f9fa1cac3c04
alphaom33 <alphaom34@gmail.com>
2024-11-09 22:20:32 -0600

    commandscheduler

--- src/main/clojure/frc/robot/core.clj
+++ src/main/clojure/frc/robot/core.clj
@@ -1,12 +1,14 @@
 (ns frc.robot.core
-  (:import [edu.wpi.first.wpilibj RobotBase TimedRobot])
+  (:import [edu.wpi.first.wpilibj TimedRobot])
+  (:import [edu.wpi.first.wpilibj2.command Command CommandScheduler])
   (:gen-class))
 
 (defn make-robot
   []
   (proxy [TimedRobot] []
     (robotInit [] (println "yay"))
-    (robotPeriodic [] (println "yay"))
+    (robotPeriodic []
+      (.run (CommandScheduler/getInstance)))
     (autonomousInit [] (println "yay"))
     (autonomousPeriodic [] (println "yay"))
     (teleopInit [] (println "yay"))
