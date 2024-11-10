e5b8023d548f928c68e18f2669f8945f044fd7a1
alphaom33 <alphaom34@gmail.com>
2024-11-09 22:08:25 -0600

    bit betterish

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,7 +1,5 @@
 package frc.robot;
 
-import java.util.function.Supplier;
-
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -11,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("[frc.robot.core]"));
-        TimedRobot a = (TimedRobot)Clojure.var("frc.robot.core", "make-robot").invoke();
-        RobotBase.startRobot(() -> a);
+        RobotBase.startRobot(() -> (TimedRobot)Clojure.var("frc.robot.core", "make-robot").invoke());
     }
 }
