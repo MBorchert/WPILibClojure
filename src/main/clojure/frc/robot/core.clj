55625cf563bac315ed3cb6d82b4bbb7e47e2e4f5
alphaom33 <alphaom34@gmail.com>
2024-11-10 06:33:53 -0600

    vendor deps work

--- src/main/clojure/frc/robot/core.clj
+++ src/main/clojure/frc/robot/core.clj
@@ -1,14 +1,19 @@
 (ns frc.robot.core
   (:import [edu.wpi.first.wpilibj TimedRobot])
+  (:import com.kauailabs.navx.frc.AHRS)
   (:import [edu.wpi.first.wpilibj2.command Command CommandScheduler])
   (:gen-class))
 
 (defn make-robot
   []
   (proxy [TimedRobot] []
-    (robotInit [] (println "yay"))
+    (robotInit [] 
+      (println "yay")
+      (def ahrs (new AHRS)))
+
     (robotPeriodic []
-      (.run (CommandScheduler/getInstance)))
+      (.run (CommandScheduler/getInstance))
+      (println (.getPitch ahrs)))
     (autonomousInit [] (println "yay"))
     (autonomousPeriodic [] (println "yay"))
     (teleopInit [] (println "yay"))
